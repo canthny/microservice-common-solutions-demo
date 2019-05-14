b008e13c1beb878702b220047f5de8e8843b6994
canthny <canthnyq8@126.com>
2019-05-14 16:06:06 +0800

    first commit

---
+++ src/main/java/com/canthny/microservice/common/solutions/demoroot/DemoRootApplication.java
@@ -0,0 +1,13 @@
+package com.canthny.microservice.common.solutions.demoroot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoRootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/canthny/microservice/common/solutions/demoroot/DemoRootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.canthny.microservice.common.solutions.demoroot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoRootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
